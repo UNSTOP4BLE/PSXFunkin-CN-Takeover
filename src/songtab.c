59427c511390ee060a1c8adbbb0d774497a8100a
UNSTOP4BLE <bruno.juraski1@gmail.com>
2023-04-16 19:45:05 +0200

    Update songtab.c

--- src/songtab.c
+++ src/songtab.c
@@ -8,11 +8,13 @@
 #include "stage.h"
 #include "gfx.h"
 #include "mutil.h"
+#include "timer.h"
 
 typedef struct 
 {
     fixed_t x, y;
     fixed_t iconzoom;
+    fixed_t bump;
 } Tab;
 
 Tab tab;
@@ -31,14 +33,14 @@ RECT geticon(int i)
 
 void bump(fixed_t *bump)
 {
-    if ((bump = FIXED_UNIT + FIXED_MUL(bump - FIXED_UNIT, FIXED_DEC(95,100))) <= FIXED_DEC(1003,1000))
-        bump = FIXED_UNIT;
+    if ((*bump = FIXED_UNIT + FIXED_MUL(*bump - FIXED_UNIT, FIXED_DEC(95,100))) <= FIXED_DEC(1003,1000))
+        *bump = FIXED_UNIT;
       
     if (stage.flag & STAGE_FLAG_JUST_STEP)
     { 
         //Bump screen
         if ((stage.song_step & 0xF) == 0)
-        	bump = FIXED_DEC(103,100); 
+            *bump = FIXED_DEC(103,100); 
     }
 }
 
@@ -53,10 +55,13 @@ void Tab_Init(void)
 
 void Tab_draw(void)
 {
+    fixed_t lastzoom;
+
     RECT icon = geticon((stage.mode == StageMode_Swap ? stage.player_state[0].character->health_i : stage.player_state[1].character->health_i));
     RECT_FIXED icon_dst = {FIXED_DEC(tab.x,1), FIXED_DEC(tab.y,1), FIXED_DEC(46,1), FIXED_DEC(46,1)};
-    bump(&tab.iconzoom);
-
+    tab.iconzoom += tab.bump - (lastzoom - tab.iconzoom)
+    tab.iconzoom += Lerp(tab.iconzoom, FIXED_DEC(timer_dt,1), FIXED_DEC(10,100));
+    bump(&bump);
     RECT bar_fill = {252, 252, 1, 1};
     if (stage.song_step >= 20 && !stage.paused)
         tab.x += Lerp(FIXED_DEC(tab.x, 1), FIXED_DEC(-screen.SCREEN_WIDTH + -40,1), FIXED_DEC(6,100)) / 1024;
@@ -67,5 +72,7 @@ void Tab_draw(void)
     Stage_DrawTex(&stage.tex_hud1, &icon, &icon_dst, tab.iconzoom);
     stage.font_cdr.draw_col(&stage.font_cdr, stage.credit, FIXED_DEC(tab.x + 155,1), FIXED_DEC(tab.y + 13,1), FontAlign_Right, stage.tr, stage.tg, stage.tb);
     Stage_BlendTex(&stage.tex_hud0, &bar_fill, &bar_dst, stage.bump, 1);
+
+    lastzoom = tab.iconzoom;
 
 }
