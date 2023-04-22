f1256864aa66e5fa63850af1b3c2bdf8117482ac
UNSTOP4BLE <bruno.juraski1@gmail.com>
2023-04-22 13:56:36 +0200

    Update audio_def.h

--- src/audio_def.h
+++ src/audio_def.h
@@ -8,7 +8,7 @@ typedef struct
 
 static const XA_TrackDef xa_tracks[] = {
     //MENU.XA
-    {XA_Menu, XA_LENGTH(11300)}, //XA_GettinFreaky
+    {XA_Menu, XA_LENGTH(15000)}, //XA_GettinFreaky
     {XA_Menu, XA_LENGTH(3800)},  //XA_GameOver
     //WEEK1A.XA
     {XA_Week1A, XA_LENGTH(16032)}, //XA_Bopeebo
